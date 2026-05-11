e59e8af2f25d135eaf5e0919a3a949406b9e3ffe
Lucas Luisetti <lucasluisetti@gmail.com>
2026-05-11 11:01:33 +0200

    chore: remove unused files

--- styles/glass.ts
+++
@@ -1,8 +0,0 @@
-export const glassStyle = {
-  background: "rgb(0 0 0 / 0.25)",
-  backdropFilter: "blur(5px)",
-  WebkitBackdropFilter: "blur(5px)",
-  border: "none",
-  boxShadow: "0 10px 40px rgb(0 0 0 / 0.35), inset 0 1px 0 rgb(255 255 255 / 0.08)",
-  borderRadius: "12px",
-};
